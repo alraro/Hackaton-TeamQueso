5dc780749816c56f6b740a44906737d872bf7a25
alraro <alfonsoramosrojas@gmail.com>
2024-10-18 10:25:31 +0200

    Names changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
+import Header from "./components/Header";
 import Home from './pages/Home';
 import SearchByDistance from './pages/Distance';
 import SearchReservoir from './pages/Sort';
